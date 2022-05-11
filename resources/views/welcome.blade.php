4a9ef753178e26086599892edc1aa8240baefc6b
hiroto-tetsuka <hiroto-tetsuka@fandmnet.com>
2022-05-11 00:28:04 +0000

    before class delete

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,16 +1,18 @@
 @extends('layouts.app')
 
 @section('content')
+    {{-- ログインが確認できたら --}}
     @if(Auth::check())
         <div class="row">
             <aside class="col-sm-4">
                 <div class="card">
                     <div class="card-header">
-                        <h3 class="card-title">{{ Auth::user()->name }}</h3>
+                        {{-- ログインしたユーザのユーザ名を表示 --}}
+                        <h3>{{ Auth::user()->name }}</h3>
                     </div>
                     <div class="card-body">
-                        {{-- 認証済みユーザのメールアドレスをもとにGravatarを取得して表示 --}}
-                        <img class="rounded img-fluid" src="{{ Gravatar::get(Auth::user()->email, ['size' => 100]) }}" alt="">
+                        {{-- ログインしたユーザのアイコンを表示 --}}
+                        <img src="{{asset('storage/sample.jpeg')}}" alt="">
                     </div>
                 </div>
             </aside>
