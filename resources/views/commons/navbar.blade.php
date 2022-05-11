778c959a479881cb7091a6aa163197c39b29cb42
hiroto-tetsuka <hiroto-tetsuka@fandmnet.com>
2022-05-11 01:45:24 +0000

    before edit route

--- resources/views/commons/navbar.blade.php
+++ resources/views/commons/navbar.blade.php
@@ -1,29 +1,23 @@
 <header>
     <nav class="header-nav">
         {{-- トップページへのリンク --}}
-        <a class="nav-logo" href="/"><img src="{{asset('img/logo.png')}}"></a>
-{{--
-        <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#nav-bar">
-            <span class="navbar-toggler-icon"></span>
-        </button>
---}}
-{{--        <div class="collapse navbar-collapse" id="nav-bar">--}}
-{{--            <ul class="navbar-nav mr-auto"></ul>--}}
-            <ul class="navbar-nav">
-                @if (Auth::check())
-                    {{-- ユーザ一覧 --}}
-                    <li class="nav-item">{!! link_to_route('users.index', 'ユーザ一覧', [], ['class' => 'nav-link']) !!}</li>
-                    {{-- 投稿ボタン --}}
-                    <li class="nav-item">{!! link_to_route('posts.create', '投稿', [], ['class' => 'nav-link']) !!}</li>
-                    {{-- マイページ --}}
-                    <li class="nav-item">{!! link_to_route('users.show', 'マイページ', ['user' => Auth::id()], ['class' => 'nav-link']) !!}</li>
-                @else
-                    {{-- ユーザ登録ページへのリンク --}}
-                    <li class="nav-item">{!! link_to_route('signup.get', '新規登録', [], ['class' => 'nav-link']) !!}</li>
-                    {{-- ログインページへのリンク --}}
-                    <li class="nav-item">{!! link_to_route('login', 'ログイン', [], ['class' => 'nav-link']) !!}</li>
-                @endif
-            </ul>
-{{--        </div>--}}
+        <a href="/"><img src="{{asset('img/logo.png')}}" class="nav-logo" alt=""></a>
+        <ul class="navbar-nav">
+            @if (Auth::check())
+                {{-- ユーザ一覧 --}}
+                <a href="{{asset('')}}" class="nav-link">ユーザ一覧</a>
+                <li>{!! link_to_route('users.index', 'ユーザ一覧', [], ['class' => 'nav-link']) !!}</li>
+                {{-- 投稿ボタン --}}
+                <li>{!! link_to_route('posts.create', '投稿', [], ['class' => 'nav-link']) !!}</li>
+                {{-- マイページ --}}
+                <li>{!! link_to_route('users.show', 'マイページ', ['user' => Auth::id()], ['class' => 'nav-link']) !!}</li>
+            @else
+                {{-- 新規登録ページへのリンク --}}
+                <li><a href="{{asset('signup')}}" class="nav-link">新規登録</a></li>
+                {{-- ログインページへのリンク --}}
+                <li><a href="{{asset('login')}}" class="nav-link">ログイン</a></li>
+                
+            @endif
+        </ul>
     </nav>
 </header>
