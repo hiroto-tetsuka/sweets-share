55f4d7216a8b3cb20a5625d300d4ce7ff84e1c04
hiroto-tetsuka <hiroto-tetsuka@fandmnet.com>
2022-05-18 00:35:58 +0000

    error message

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -51,7 +51,9 @@
             @if (count($errors) > 0)
                 <ul>
                     @foreach ($errors->all() as $error)
+                    <div class="errorMessage">
                         <li>{{ $error }}</li>
+                    </div>
                     @endforeach
                 </ul>
             @endif
